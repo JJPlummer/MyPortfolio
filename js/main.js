f474f7eef1dfcf0e5c139b9683d2a4ddab98ff3f
James Plummer <jamesjplummer@gmail.com>
2025-06-27 15:10:16 -0400

    added javascript and started junior year

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// === NAVBAR INJECTION ===
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('../navbar.html')
+    .then(res => res.text())
+    .then(data => {
+      const placeholder = document.getElementById('navbar-placeholder');
+      if (placeholder) {
+        placeholder.innerHTML = data;
+      }
+    });
+
+    //Slideshow initialization
+    slideIndex=1;
+    showSlides(slideIndex);
+});
+
+// === PDF TOGGLE ===
+function togglePDF() {
+  const pdfViewer = document.getElementById("pdf-viewer");
+  if (pdfViewer) {
+    pdfViewer.classList.toggle("hidden");
+  }
+}
+
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  
+}
